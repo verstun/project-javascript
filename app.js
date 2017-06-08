afe3feee42290a6cdd9694dc320e7bf896fac8e9
verstun <svetazavolokina81@gmail.com>
2017-06-08 13:53:34 -0400

    create initialisation function

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-})();
-
-
-
-
-var UIController = (function(){
 
     //some code
 
@@ -23,12 +8,90 @@ var UIController = (function(){
 
 
 
-var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
+// UI CONTROLLER
+var UIController = (function(){
+
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
     return {
-        anotherPublic: function() {
-            console.log(z);
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // will be either inc or exp
+                decrription: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value,
+            }
+
+        },
+        getDOMstrings: function() {
+            return DOMStrings;
         }
     };
 
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+
+    var setupEventListeners = function() {
+
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+
+        });
+    };
+
+
+
+    var ctrlAddItem = function(){
+
+        // 1. Get the field input data
+
+        var input = UIController.getInput();
+        console.log(input);
+
+        // 2. Add item to budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on UI
+
+    }
+
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    }
+
+
 })(budgetController, UIController);
+
+controller.init();
+
+
+
+
+
+
+
+
+
+
+
